9b3b8763eb2fa63fbac89992ce1e2e4d7d42baf4
Jonas Faber <72028327+Jonohas@users.noreply.github.com>
2022-12-29 12:30:54 +0100

    Add files via upload

--- insall.ps1
+++ insall.ps1
@@ -1,5 +1,7 @@
 Write-Output "Setting ExecutionPolicy"
 Set-ExecutionPolicy AllSigned
+
+# Make sure LongPathNames is enabled
 Write-Output "Installing chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Write-Output "Installing packages"
