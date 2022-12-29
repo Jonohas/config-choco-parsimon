0d69bd344754574f1f2f5859da92aa9192382739
Jonas Faber <72028327+Jonohas@users.noreply.github.com>
2022-12-29 12:24:13 +0100

    Add files via upload

---
+++ insall.ps1
@@ -0,0 +1,7 @@
+Write-Output "Setting ExecutionPolicy"
+Set-ExecutionPolicy AllSigned
+Write-Output "Installing chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Write-Output "Installing packages"
+choco install .\choco-parsimon.config
+Write-Output "done"
